c07839aa51be0c41f25dbdbccd182de4a6c6b718
Cale Davis <daviscale@gmail.com>
2016-06-04 09:59:24 -0400

    use timestamp in target directory

--- CopyDocs/Program.cs
+++ CopyDocs/Program.cs
@@ -9,40 +9,62 @@ namespace CopyDocs
         {
             if (args.Length == 2)
             {
-                string sourceDirectoryName = args[0];
-                string destinationDirectoryName = args[1];
-                DirectoryCopy(sourceDirectoryName, destinationDirectoryName);
+                string sourcePath = args[0];
+                string destinationPathPrefix = args[1];
+                string destinationPath = CreateDestinationPath(sourcePath, destinationPathPrefix);
+
+                DirectoryCopy(sourcePath, destinationPath);
             } else
             {
-                Console.WriteLine("Please supply a source directory name and a target directory name as arguments");
+                Console.WriteLine("Please supply a source directory and a target directory as arguments");
             }
         }
+        
+        private static String CreateDestinationPath(string sourcePath, string destinationPathPrefix)
+        {
+            string folderToCopy = new DirectoryInfo(sourcePath).Name;
+            string currentDateTime = DateTime.Now.ToString();
+            string backupFolderName = $"{folderToCopy}-{currentDateTime}";
+            return Path.Combine(destinationPathPrefix, backupFolderName);
+        }
 
-        private static void DirectoryCopy(string sourceDirName, string destinationDirName)
+        private static String CreateTimestampString()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            var year = currentDateTime.Year.ToString();
+            var month = currentDateTime.Month.ToString();
+            var day = currentDateTime.Day.ToString();
+            var hour = currentDateTime.Hour.ToString();
+            var minute = currentDateTime.Minute.ToString();
+            var second = currentDateTime.Second.ToString();
+            return $"{year}{month}{day}{hour}{minute}{second}";
+        }
+
+        private static void DirectoryCopy(string sourcePath, string destinationPath)
         {
             // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo dir = new DirectoryInfo(sourcePath);
 
             if (!dir.Exists)
             {
                 throw new DirectoryNotFoundException(
                     "Source directory does not exist or could not be found: "
-                    + sourceDirName);
+                    + sourcePath);
             }
 
             DirectoryInfo[] dirs = dir.GetDirectories();
             // If the destination directory doesn't exist, create it.
-            if (!Directory.Exists(destinationDirName))
+            if (!Directory.Exists(destinationPath))
             {
-                Directory.CreateDirectory(destinationDirName);
+                Directory.CreateDirectory(destinationPath);
             }
 
             // Get the files in the directory and copy them to the new location.
             FileInfo[] files = dir.GetFiles();
             foreach (FileInfo file in files)
             {
-                string oldPath = Path.Combine(sourceDirName, file.Name);
-                string newPath = Path.Combine(destinationDirName, file.Name);
+                string oldPath = Path.Combine(sourcePath, file.Name);
+                string newPath = Path.Combine(destinationPath, file.Name);
                 Console.WriteLine($"Copying {oldPath} to {newPath}");
                 file.CopyTo(newPath, false);
             }
@@ -50,7 +72,7 @@ namespace CopyDocs
             // If copying subdirectories, copy them and their contents to new location.
             foreach (DirectoryInfo subdir in dirs)
             {
-                string temppath = Path.Combine(destinationDirName, subdir.Name);
+                string temppath = Path.Combine(destinationPath, subdir.Name);
                 DirectoryCopy(subdir.FullName, temppath);
             }
         }
